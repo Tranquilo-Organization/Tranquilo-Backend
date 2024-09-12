cb4251af87b38022c3824ea0b2e2cb3e94afb1fd
moustafanafeaa <moustafa.anafea@gmail.com>
2024-09-12 13:18:38 +0300

    Add AutoMapper File In BLL

---
+++ TranquiloSystem/TranquiloSystem.BLL/AutoMapper/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranquiloSystem.BLL.AutoMapper
+{
+	public class MappingProfile : Profile
+	{
+		public MappingProfile()
+		{
+			
+		}
+	}
+}
--- TranquiloSystem/TranquiloSystem.BLL/obj/Debug/net8.0/TranquiloSystem.BLL.AssemblyInfo.cs
+++ TranquiloSystem/TranquiloSystem.BLL/obj/Debug/net8.0/TranquiloSystem.BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TranquiloSystem.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03e810532da004e7a8ab2f1cc3fb9a044e1f975c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f50b8ddf2020b098595424adccbe15ed527efb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TranquiloSystem.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TranquiloSystem.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
