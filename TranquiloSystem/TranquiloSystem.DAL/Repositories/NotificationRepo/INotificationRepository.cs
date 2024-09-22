3f8f4ac69ec124878c8548b799c38fe4aa04897c
moustafanafeaa <moustafa.anafea@gmail.com>
2024-09-22 09:18:40 +0300

    Add files in NotificationRepo in DAL

---
+++ TranquiloSystem/TranquiloSystem.DAL/Repositories/NotificationRepo/INotificationRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TranquiloSystem.DAL.Data.Models;
+
+namespace TranquiloSystem.DAL.Repositories.NotificarioRepo
+{
+	public interface INotificationRepository
+	{
+		Task AddAsync(Notification notification);
+		Task<Notification> GetByIdAsync(int id);
+		Task<List<Notification>> GetByUserIdAsync(string userId);
+		Task UpdateAsync(Notification notification);
+	}
+}
---
+++ TranquiloSystem/TranquiloSystem.DAL/Repositories/NotificationRepo/NotificationRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TranquiloSystem.DAL.Data.DbHelper;
+using TranquiloSystem.DAL.Data.Models;
+
+namespace TranquiloSystem.DAL.Repositories.NotificarioRepo
+{
+	public class NotificationRepository : INotificationRepository
+	{
+		private readonly TranquiloContext _context;
+
+		public NotificationRepository(TranquiloContext context)
+		{
+			_context = context;
+		}
+		public async Task AddAsync(Notification notification)
+		{
+			await _context.Notifications.AddAsync(notification);
+			await _context.SaveChangesAsync();
+		}
+
+		public async Task<Notification> GetByIdAsync(int id)
+		{
+			return await _context.Notifications.FindAsync(id);
+		}
+
+		public async Task<List<Notification>> GetByUserIdAsync(string userId)
+		{
+			return await _context.Notifications
+				.Where(n => n.UserId == userId)
+				.ToListAsync();
+		}
+
+		public async Task UpdateAsync(Notification notification)
+		{
+			_context.Notifications.Update(notification);
+			await _context.SaveChangesAsync();
+		}
+	}
+}
