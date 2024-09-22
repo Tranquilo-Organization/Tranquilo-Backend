b454b9a6ff948a5f1994fe5f2af1bcd7460fe47c
moustafanafeaa <moustafa.anafea@gmail.com>
2024-09-22 09:17:14 +0300

    Modify RoutineRepo in DAL

--- TranquiloSystem/TranquiloSystem.DAL/Repositories/RoutineRepo/IRoutineRepository.cs
+++ TranquiloSystem/TranquiloSystem.DAL/Repositories/RoutineRepo/IRoutineRepository.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tranquilo.DAL.Data.Models;
 
 namespace Tranquilo.DAL.Repositories.RoutineRepo
 {
     public interface IRoutineRepository
     {
-    }
+		Task<IEnumerable<Routine>> GetAllAsync();
+		Task<Routine> GetByIdAsync(int id);
+		Task<ICollection<Routine>> GetByLevelIdAsync(int levelId);
+	}
 }
--- TranquiloSystem/TranquiloSystem.DAL/Repositories/RoutineRepo/RoutineRepository.cs
+++ TranquiloSystem/TranquiloSystem.DAL/Repositories/RoutineRepo/RoutineRepository.cs
@@ -1,12 +1,58 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tranquilo.DAL.Data.Models;
+using TranquiloSystem.DAL.Data.DbHelper;
 
 namespace Tranquilo.DAL.Repositories.RoutineRepo
 {
-    public class RoutineRepository:IRoutineRepository
-    {
-    }
+	public class RoutineRepository : IRoutineRepository
+	{
+		private readonly TranquiloContext _context;
+
+		public RoutineRepository(TranquiloContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<IEnumerable<Routine>> GetAllAsync()
+		{
+			var result = await _context.Routines
+				.Include(x => x.Level).AsNoTracking().ToListAsync();
+			return result;
+		}
+
+		public async Task<Routine> GetByIdAsync(int id)
+		{
+			var routine = await _context.Routines
+				.Include(x => x.Level).FirstOrDefaultAsync(x=>x.Id == id);
+			if(routine == null)
+			{
+				return null;
+			}
+			return routine;
+		}
+		public async Task<ICollection<Routine>> GetByLevelIdAsync(int levelId)
+		{
+			var level = await _context.Levels
+				.FindAsync(levelId);
+			if(level == null)
+			{
+				return null;
+			}
+
+			var routines = await _context.Routines
+				.Include(x => x.Level)
+				.Where(x=>x.LevelId == levelId).ToListAsync();
+
+			if(routines == null)
+			{
+				return null;
+			}
+			return routines;
+		}
+	}
 }
